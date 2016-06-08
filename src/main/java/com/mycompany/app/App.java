5ba74b8aaa57cc5eab5cc83acf1c2e2a60817cb2
Hercules Gomez <owenkilgannon@gmail.com>
2016-06-09 00:47:08 +0100

    update new server location

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -14,8 +14,8 @@ public class App {
 
 		// Kafka connection properties
 		Properties props = new Properties();
-		props.put("bootstrap.servers", "localhost:9092");
-		props.put("zk.connect", "localhost:2181");
+		props.put("bootstrap.servers", "192.168.0.150:9092");
+		props.put("zk.connect", "192.168.0.150:2181");
 		props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 		props.put("value.serializer", "org.apache.kafka.common.serialization.ByteArraySerializer");
 		props.put("acks", "all");
