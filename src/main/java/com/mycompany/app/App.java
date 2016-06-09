e2bef96cdf6b5857e9bc2c202d19e16065bafb57
Hercules Gomez <owenkilgannon@gmail.com>
2016-06-09 13:25:43 +0100

    latest

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -15,7 +15,7 @@ public class App {
 		// Kafka connection properties
 		Properties props = new Properties();
 		props.put("bootstrap.servers", "192.168.0.150:9092");
-		//props.put("zk.connect", "192.168.0.150:2181");
+		props.put("zk.connect", "192.168.0.150:2181");
 		props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 		props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 		props.put("acks", "all");
@@ -28,10 +28,10 @@ public class App {
 		for (int i = 0; i < 100; i++) {
 			System.out.println("Sending message: " + Integer.toString(i));
 			producer.send(
-					new ProducerRecord<String, String>("headshotevent", Integer.toString(i), Integer.toString(i)));
+					new ProducerRecord<String, String>("headshotevent", "testestestestsetsetsetsetse", "testestestestsetsetsetsetse"));
 			System.out.println("Sent message: " + Integer.toString(i));
 		}
 		producer.close();
-
+		System.out.println("Producer closed");
 	}
 }
