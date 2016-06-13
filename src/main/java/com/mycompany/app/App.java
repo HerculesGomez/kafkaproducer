6eb0b1eec991a97a38b53303ef3919aeaef18645
Hercules Gomez <owenkilgannon@gmail.com>
2016-06-13 14:40:07 +0100

    Java client now talking with kafka server

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -25,20 +25,20 @@ public class App {
 		// Kafka connection properties
 		Properties props = new Properties();
 		props.put("bootstrap.servers", "192.168.0.150:9092");
-		//props.put("zk.connect", "localhost:2181");
+		props.put("zk.connect", "localhost:2181");
 		props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 		props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 		props.put("acks", "all");
 		props.put("retries", 0);
-		//props.put("batch.size", 16384);
+		props.put("batch.size", 16384);
 		props.put("linger.ms", 1);
 
 		Producer<String, String> producer = new KafkaProducer<String, String>(props);
 
 		for (int i = 0; i < 100; i++) {
 			System.out.println("Sending message: " + Integer.toString(i));
-			producer.send(new ProducerRecord<String, String>("headshotevent", Integer.toString(i),
-					"tits"));
+			producer.send(new ProducerRecord<String, String>("test_topic", Integer.toString(i),
+					"Test string " + Integer.toString(i)));
 			System.out.println("Sent message: " + Integer.toString(i));
 		}
 		producer.close();
