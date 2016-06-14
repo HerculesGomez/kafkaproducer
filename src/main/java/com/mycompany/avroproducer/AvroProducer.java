349f1b020cdf82f62862d2c725b3862e386158fb
Hercules Gomez <owenkilgannon@gmail.com>
2016-06-14 20:20:24 +0100

    Byte consumer and producer now working

--- src/main/java/com/mycompany/avroproducer/AvroProducer.java
+++ src/main/java/com/mycompany/avroproducer/AvroProducer.java
@@ -6,127 +6,163 @@ import com.twitter.bijection.avro.GenericAvroCodecs;
 import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericData;
 import org.apache.avro.generic.GenericRecord;
-
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
-
-import kafka.serializer.DefaultDecoder;
-import kafka.serializer.StringDecoder;
-
-import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.streaming.Duration;
-import org.apache.spark.streaming.api.java.JavaPairInputDStream;
-import org.apache.spark.streaming.api.java.JavaStreamingContext;
-import org.apache.spark.streaming.kafka.KafkaUtils;
+import org.apache.kafka.common.TopicPartition;
 
 import java.util.Properties;
 import java.util.Random;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.Arrays;
 
 public class AvroProducer {
 
-	private static String[] usernames = {"herculesgomez", "billywizz", "charliechaplin", "bennybronco", "jimmyfish"};
-	private static String[] maps = {"swamp", "cesspit", "bridge", "lake", "farm"};
-	private static String[] gametypes = {"free for all", "team deathmatch", "capture the flag"};
-	
-	public static final String USER_SCHEMA = "{"
-            + "\"type\":\"record\","
-            + "\"name\":\"headshotevent\","
-            + "\"fields\":["
-            + "  { \"name\":\"username\", \"type\":\"string\" },"
-            + "  { \"name\":\"gametype\", \"type\":\"string\" },"
-            + "  { \"name\":\"map\", \"type\":\"string\" },"
-            + "  { \"name\":\"timestamp\", \"type\":\"long\" }"
-            + "]}";
-	
-	public static void main(String[] args) {
-		produce();
-		consume();
-		
+	private static String[] usernames = { "herculesgomez", "billywizz", "charliechaplin", "bennybronco", "jimmyfish" };
+	private static String[] maps = { "swamp", "cesspit", "bridge", "lake", "farm" };
+	private static String[] gametypes = { "free for all", "team deathmatch", "capture the flag" };
+
+	public static final String USER_SCHEMA = "{" + "\"type\":\"record\"," + "\"name\":\"headshotevent\","
+			+ "\"fields\":[" + "{\"name\":\"username\",\"type\":\"string\"},"
+			+ "{\"name\":\"gametype\",\"type\":\"string\"}," + "{\"name\":\"map\",\"type\":\"string\"},"
+			+ "{\"name\":\"timestamp\",\"type\":\"long\"}" + "]}";
+
+	private static Injection<GenericRecord, byte[]> recordInjection;
+
+	static {
+		Schema.Parser parser = new Schema.Parser();
+		Schema schema = parser.parse(USER_SCHEMA);
+		recordInjection = GenericAvroCodecs.toBinary(schema);
 	}
 
-	private static void produce() {
-		System.out.println("Starting Avro Producer");
-		
+	public static void main(String[] args) {
+		produceAvroBytes();
+		// produceAvroStrings();
+		// consumeSpark();
+		consumeAvroBytes();
+	}
+
+	private static void consumeAvroBytes() {
+		System.out.println("\n\nStarting Avro Byte Consumer");
+
 		Properties props = new Properties();
-        props.put("bootstrap.servers", "192.168.0.150:9092");
-        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        props.put("value.serializer", "org.apache.kafka.common.serialization.ByteArraySerializer");
+		props.put("bootstrap.servers", "192.168.0.150:9092");
+		String consumeGroup = "cg1";
+		props.put("group.id", consumeGroup);
+		props.put("enable.auto.commit", "true");
+		props.put("auto.offset.reset", "earliest");
+		props.put("auto.commit.interval.ms", "100");
+		props.put("heartbeat.interval.ms", "3000");
+		props.put("session.timeout.ms", "30000");
+		props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+		props.put("value.deserializer", "org.apache.kafka.common.serialization.ByteArrayDeserializer");
 
-        Schema.Parser parser = new Schema.Parser();
-        Schema schema = parser.parse(USER_SCHEMA);
-        Injection<GenericRecord, byte[]> recordInjection = GenericAvroCodecs.toBinary(schema);
+		KafkaConsumer<String, byte[]> consumer = new KafkaConsumer<String, byte[]>(props);
 
-        KafkaProducer<String, byte[]> producer = new KafkaProducer<>(props);
-        
-        for (int i = 0; i < 10; i++) {
-        	System.out.println("Sending record " + Integer.toString(i));
-        	Random random = new Random();
-        	java.util.Date today = new java.util.Date();
-            java.sql.Timestamp ts1 = new java.sql.Timestamp(today.getTime());
-            long tsTime1 = ts1.getTime();
-        	
-            GenericData.Record avroRecord = new GenericData.Record(schema);
-            avroRecord.put("username", usernames[random.nextInt(usernames.length)]);
-            avroRecord.put("gametype", gametypes[random.nextInt(gametypes.length)]);
-            avroRecord.put("map", maps[random.nextInt(maps.length)]);
-            avroRecord.put("timestamp", tsTime1);
+		// Assign to specific topic and partition, subscribe could be used here
+		// to subscribe to all topic.
+		consumer.assign(Arrays.asList(new TopicPartition("headshotevent4", 0)));
 
-            byte[] bytes = recordInjection.apply(avroRecord);
+		while (true) {
 
-            ProducerRecord<String, byte[]> record = new ProducerRecord<>("headshotevent2", bytes);
-            producer.send(record);
+			ConsumerRecords<String, byte[]> records = consumer.poll(100);
 
-            try {
-				Thread.sleep(250);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+			long lastOffset = 0;
+
+			for (ConsumerRecord<String, byte[]> record : records) {
+				GenericRecord convertedrecord = recordInjection.invert(record.value()).get();
+				System.out.println(convertedrecord);
+				lastOffset = record.offset();
 			}
 
-        }
-        producer.close();
-        System.out.println("Closed Avro Producer");
-		
+			System.out.println("lastOffset read: " + lastOffset);
+			consumer.commitSync();
+
+		}
+
 	}
 
-	private static void consume() {
-		System.out.println("\n.......\n\nStarting Avro Consumer");
-		
-		SparkConf conf = new SparkConf()
-                .setAppName("kafka-sandbox")
-                .setMaster("local[*]");
-        JavaSparkContext sc = new JavaSparkContext(conf);
-        JavaStreamingContext ssc = new JavaStreamingContext(sc, new Duration(2000));
+	private static void produceAvroBytes() {
+		System.out.println("\n\nStarting Avro Producer");
 
-        Set<String> topics = Collections.singleton("headshotevent2");
-        Map<String, String> kafkaParams = new HashMap<>();
-        kafkaParams.put("metadata.broker.list", "192.168.0.150:9092");
-        
-        JavaPairInputDStream<String, byte[]> directKafkaStream = KafkaUtils.createDirectStream(ssc,
-                String.class, byte[].class, StringDecoder.class, DefaultDecoder.class, kafkaParams, topics);
+		Properties props = new Properties();
+		props.put("bootstrap.servers", "192.168.0.150:9092");
+		props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		props.put("value.serializer", "org.apache.kafka.common.serialization.ByteArraySerializer");
 
-        directKafkaStream.foreachRDD(rdd -> {
-            rdd.foreach(avroRecord -> {
-                Schema.Parser parser = new Schema.Parser();
-                Schema schema = parser.parse(USER_SCHEMA);
-                Injection<GenericRecord, byte[]> recordInjection = GenericAvroCodecs.toBinary(schema);
-                GenericRecord record = recordInjection.invert(avroRecord._2).get();
+		Schema.Parser parser = new Schema.Parser();
+		Schema schema = parser.parse(USER_SCHEMA);
+		Injection<GenericRecord, byte[]> recordInjection = GenericAvroCodecs.toBinary(schema);
 
-                System.out.println("username= " + record.get("username")
-                        + ", gametype= " + record.get("gametype")
-                        + ", map= " + record.get("map")
-                        + ", timestamp=" + record.get("timestamp"));
-            });
-        });
+		KafkaProducer<String, byte[]> producer = new KafkaProducer<>(props);
 
-        ssc.start();
-        ssc.awaitTermination();
-        System.out.println("\n.......\nClosed Avro Consumer");
-        
+		for (int i = 0; i < 1000; i++) {
+			System.out.println("Sending record " + Integer.toString(i));
+			Random random = new Random();
+			java.util.Date today = new java.util.Date();
+			java.sql.Timestamp ts1 = new java.sql.Timestamp(today.getTime());
+			long tsTime1 = ts1.getTime();
+
+			GenericData.Record avroRecord = new GenericData.Record(schema);
+			avroRecord.put("username", usernames[random.nextInt(usernames.length)]);
+			avroRecord.put("gametype", gametypes[random.nextInt(gametypes.length)]);
+			avroRecord.put("map", maps[random.nextInt(maps.length)]);
+			avroRecord.put("timestamp", tsTime1);
+
+			byte[] bytes = recordInjection.apply(avroRecord);
+
+			ProducerRecord<String, byte[]> record = new ProducerRecord<>("headshotevent4", bytes);
+			producer.send(record);
+
+		}
+		producer.close();
+		System.out.println("Closed Avro Producer");
+
+	}
+
+	private static void produceAvroStrings() {
+		System.out.println("Starting Avro Producer");
+
+		Properties props = new Properties();
+		props.put("bootstrap.servers", "192.168.0.150:9092");
+		props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+		Schema.Parser parser = new Schema.Parser();
+		Schema schema = parser.parse(USER_SCHEMA);
+
+		KafkaProducer<String, String> producer = new KafkaProducer<>(props);
+
+		for (int i = 0; i < 1000; i++) {
+			System.out.println("Sending record " + Integer.toString(i));
+			Random random = new Random();
+			java.util.Date today = new java.util.Date();
+			java.sql.Timestamp ts1 = new java.sql.Timestamp(today.getTime());
+			long tsTime1 = ts1.getTime();
+
+			GenericData.Record avroRecord = new GenericData.Record(schema);
+			avroRecord.put("username", usernames[random.nextInt(usernames.length)]);
+			avroRecord.put("gametype", gametypes[random.nextInt(gametypes.length)]);
+			avroRecord.put("map", maps[random.nextInt(maps.length)]);
+			avroRecord.put("timestamp", tsTime1);
+
+			ProducerRecord<String, String> record = new ProducerRecord<>("headshotevent2", avroRecord.toString());
+			producer.send(record);
+
+			// try {
+			// Thread.sleep(250);
+			// } catch (InterruptedException e) {
+			// e.printStackTrace();
+			// }
+
+		}
+		producer.close();
+		System.out.println("Closed Avro Producer");
+
+	}
+
+	private static void consumeSpark() {
+		// TODO
 	}
 }
